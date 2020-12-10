c263bb9e71d9f204b1a693209c6f44aa5841d7d5
Void <wangakanjala@gmail.com>
2020-12-10 20:37:00 +0200

    updated JobType column

--- assets/php/postjob.php
+++ assets/php/postjob.php
@@ -7,11 +7,12 @@ if(isset($_POST['post'])){
 	$title = escTxt($connection, $_POST['title']);
 	$location = escTxt($connection, $_POST['location']);
 	$minidesc = escTxt($connection, $_POST['minidesc']);
+	$jobType = escTxt($connection, $_POST['jobType']);
 	$description = escTxt($connection, $_POST['description']);
 	$closing = escTxt($connection, $_POST['closingDate']);
 	$date = date("Y-m-d");
 	// insert into vacancy table
-	$query = "INSERT INTO `vacancies` VALUES(NULL, $ID, '$date', '$closing', '$title', '$location', '$description', '$minidesc', 'Pending')";
+	$query = "INSERT INTO `vacancies` VALUES(NULL, $ID, '$date', '$closing', '$title', '$location', '$description', '$minidesc', 'Pending', '$jobType')";
 	$result = $connection -> query($query);
 	if($result){
 		redirect_to("../../employer.php#posted?reply=success"); // redirect user to home page
